7d0d909d4dc2dcbfdbc59f6d84cf46992cc71464
joshua-miles <joshua@christetulit.org>
2019-07-15 08:49:54 -0500

    -

--- src/index.js
+++ src/index.js
@@ -1,12 +1,83 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+document.addEventListener('DOMContentLoaded', function(){
+    
+    let character;
+    
+    fetch('http://localhost:3000/players/1')
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(result){
+            character = new PlayableCharacter( result.name, result.x, result.y )
+        })
 
-ReactDOM.render(<App />, document.getElementById('root'));
+    fetch('http://localhost:3000/npcs')
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(results){
+            results.forEach(result => {
+                new NonPlayableCharacter( result.name, result.x, result.y )
+            })    
+        })
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+    let speedInput = document.querySelector('.speed-input')
+    speedInput.addEventListener('change', function(e){        
+        character.speed = parseInt(speedInput.value)
+    })
+
+
+    let abilityDropDown = document.querySelector('.ability-drop-down')
+    abilityDropDown.addEventListener('change', () => {
+        
+    })
+
+    let saveButton = document.querySelector('.save-button')
+    saveButton.addEventListener('click', function(){
+        // VERB: POST
+        // PATH: http://localhost:3000/characterData
+        // body: { x, y }
+        fetch('http://localhost:3000/players/1', {
+            method: 'PATCH',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                x: parseInt(character.element.style.left),
+                y: parseInt(character.element.style.bottom)
+            })
+        })
+    })
+
+})
+
+
+function oneOnOne(searchTerm){
+    // Remove the element from the page
+    let student = Character.all.find( character => character.name === searchTerm )
+    student.element.remove()
+    
+    // Remove the element from the array
+    Character.all = Character.all.filter( character => character != student )
+}
+
+
+let isNonPlayable = (character) => character instanceof NonPlayableCharacter
+let to = (propertyName) => (character) => character[propertyName]
+
+function stillConfused(){
+    return Character.all
+            .filter(isNonPlayable)
+            .map( to('name') )
+}
+
+function groupReview(){
+    Character.all.forEach(function(student){
+        if(
+            student instanceof NonPlayableCharacter 
+        ){
+            student.element.remove()
+        }
+    })
+    Character.all = []
+}
